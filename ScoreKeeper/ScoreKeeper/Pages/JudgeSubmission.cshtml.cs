c4c2b12506263e4e8c9067765943b1faec56eb11
joeytarnowski <joeytarnowski@gmail.com>
2023-10-05 08:43:52 -0500

    Added Judge Pages

---
+++ ScoreKeeper/ScoreKeeper/Pages/JudgeSubmission.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScoreKeeper.Pages
+{
+    public class JudgeSubmissionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
